a0e538c6cf60ad8e096fb7d5acd732df8c1e8a6d
Ben Wiklund <ben@daisyowl.com>
2013-12-31 18:25:06 -0800

    starting instructions

--- vm.c
+++ vm.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #define MEM_SIZE 65536
 
+typedef enum {
+  MOVE,
+  ADD,
+  JUMP
+} instruction;
+
 struct Machine {
   int *mem;
   int ip;
@@ -9,4 +15,11 @@ struct Machine {
 int main(){
   struct Machine machine = {};
   int mem[ MEM_SIZE ] = {[0 ... MEM_SIZE-1] = 0};
+  machine.mem = mem;
+
+  while( machine.ip < MEM_SIZE ){
+    int instruction = machine.mem[ machine.ip++ ];
+  }
+
+  printf("Program exited: %d",machine.mem[0]);
 }
