9fb046963394b9b1191e5f349b28564967690a48
Ben Wiklund <ben@daisyowl.com>
2014-01-01 20:16:32 -0800

    tiny instruction set test

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc vm.c && ./a.out
--- vm.c
+++ vm.c
@@ -2,24 +2,56 @@
 #define MEM_SIZE 65536
 
 typedef enum {
-  MOVE,
-  ADD,
-  JUMP
-} instruction;
+  JUMP,  // move to the address in the next arg
+  ADD,   // add the value in the next arg's adress to ACC
+  SET,   // set the ACC to be a value in the next arg
+  WRITE, // write ACC to the address in the next arg
+  READ   // read the adress in the next arg to ACC 
+} t_instruction;
 
 struct Machine {
   int *mem;
   int ip;
+  int acc;
 };
 
 int main(){
   struct Machine machine = {};
   int mem[ MEM_SIZE ] = {[0 ... MEM_SIZE-1] = 0};
   machine.mem = mem;
+  
+  int i = 0;
+  machine.mem[i++] = SET;
+  machine.mem[i++] = 1337;
+  machine.mem[i++] = WRITE;
+  machine.mem[i++] = 0;
+  machine.mem[i++] = JUMP;
+  machine.mem[i++] = MEM_SIZE;
 
   while( machine.ip < MEM_SIZE ){
-    int instruction = machine.mem[ machine.ip++ ];
+    t_instruction instruction = machine.mem[ machine.ip++ ];
+    printf("%d\n",machine.ip);
+    printf("%d\n",machine.acc);
+    switch( instruction ){
+      case JUMP:
+        machine.ip = machine.mem[ machine.ip ];
+        break;
+      case ADD:
+        machine.acc += machine.mem[ machine.ip++ ];
+        break;
+      case SET:
+        machine.acc = machine.mem[ machine.ip++ ];
+        break;
+      case WRITE:
+        machine.mem[ machine.mem[ machine.ip++ ] ] = machine.acc;
+        break;
+      case READ:
+        machine.acc = machine.mem[ machine.ip++ ];
+        break;
+    }
   }
 
-  printf("Program exited: %d",machine.mem[0]);
-}
+  printf("Exit code: %d\n\n",machine.mem[0]);
+
+  return 1;
+} 
